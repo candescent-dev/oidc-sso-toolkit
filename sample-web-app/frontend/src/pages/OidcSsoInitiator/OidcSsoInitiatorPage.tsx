d59969439ca3e7a59e7121c7e5e8d68d75d53c24
Shruthi <shruthi.g@candescent.com>
2025-11-14 11:46:43 +0530

    DEVEX-2190 | Shruthi G | Creation of OIDC start page

--- sample-web-app/frontend/src/pages/OidcSsoInitiator/OidcSsoInitiatorPage.tsx
+++ sample-web-app/frontend/src/pages/OidcSsoInitiator/OidcSsoInitiatorPage.tsx
@@ -1,13 +1,38 @@
-import React, { FC } from 'react';
-import "./OidcSsoInitiatorPage.css"
-// import { useDispatch } from 'react-redux';
+import React, { FC, useState } from "react";
+import { Link } from "@mui/material";
+import "./OidcSsoInitiatorPage.css";
+import { useAppSelector } from '../../app/hooks';
+
 
 const OidcSsoInitiatorPage: FC = () => {
-  // const dispatch = useDispatch();
-  
+  const { openOption, iframeSettings, initUrl } = useAppSelector((state: any) => state.session);
+   const [showIframe, setShowIframe] = useState(false);
+
+  const handleClick = () => {
+    // Show iframe only when link is clicked
+    setShowIframe(true);
+  };
+  console.log(iframeSettings,'iframeSettingsiframeSettings');
   return (
     <main className="oidc-sso-initiator-page">
-      <h1>Start OIDC SSO</h1>
+      <div>
+        <h3>Sign in Securely</h3>
+        <Link
+          href={initUrl}
+          underline="always"
+          color="primary"
+          target={openOption === 'newTab' ? '_blank' : 'OIDC_iFrame'}
+          rel="noopener noreferrer"
+          onClick={handleClick}
+        >
+          Start OIDC SSO
+        </Link>
+        {(iframeSettings && showIframe) &&<iframe
+          name="OIDC_iFrame"
+          title="OIDC SSO Frame"
+          style={{ width: "100%", height: "500px", border: "1px solid #ccc", marginTop: "20px" }}
+        ></iframe>}
+      </div>
     </main>
   );
 };
