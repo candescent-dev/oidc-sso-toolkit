d0b0fb30f8e0cfcd7c2424b0c5349226fbbd1a2e
Pinkar Kumari <pinkar.kumari@candescent.com>
2025-12-11 16:34:21 +0530

    Sample Web App: Backend - E2E Module - Test Case Fix

--- sample-web-app/backend/src/e2eTest/e2eTest.controller.spec.ts
+++ sample-web-app/backend/src/e2eTest/e2eTest.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { E2ETestController } from './e2eTest.controller';
 import { E2ETestService } from './e2eTest.service';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
 
 describe('E2ETestController', () => {
   let controller: E2ETestController;
@@ -8,7 +9,17 @@ describe('E2ETestController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [E2ETestController],
-      providers: [E2ETestService],
+      providers: [
+        E2ETestService,
+        {
+          provide: CACHE_MANAGER,
+          useValue: {
+            get: jest.fn(),
+            set: jest.fn(),
+            del: jest.fn(),
+          }, // simple mock for cache manager
+        },
+      ],
     }).compile();
     controller = module.get<E2ETestController>(E2ETestController);
   });
